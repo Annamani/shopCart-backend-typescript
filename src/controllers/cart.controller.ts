89c916eb59b8a48b1d795227bbd3927785b44e95
Annamani <annamanik.vit@gmail.com>
2026-05-21 12:34:32 +0200

    checking product id existing or not when adding items to cart

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -16,26 +16,25 @@ export const createNewCart = (_req: Request, res: Response): void => {
   res.status(201).json(cart);
 };
 export const addItem = (req: Request, res: Response): void => {
-  const { cartId, productId, quantity } = req.body;
-
-  const updatedCart = addItemToCart(cartId, productId, quantity);
-
-  if (!updatedCart) {
-    res.status(404).json({ error: "Cart not found" });
-    return;
+  try {
+    const { cartId, productId, quantity } = req.body;
+    const updatedCart = addItemToCart(cartId, productId, quantity);
+    if (!updatedCart) res.status(404).json({ error: "Cart not found" });
+    res.status(200).json(updatedCart);
+  } catch (error) {
+    res.status(400).json(error);
   }
-
-  res.json(updatedCart);
 };
 export const getTotal = (req: Request, res: Response): void => {
-  const { cartId } = req.body;
-
-  const total = calculateCartTotal(cartId);
-
-  if (total === null) {
-    res.status(404).json({ error: "Cart not found" });
-    return;
+  try {
+    const { cartId } = req.body;
+    const total = calculateCartTotal(cartId);
+    if (!total) {
+      res.status(404).json({ error: "Cart not found" });
+      return;
+    }
+    res.status(200).json({ total });
+  } catch (error) {
+    res.status(404).json(error);
   }
-
-  res.json({ total });
 };
